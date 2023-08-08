98ad25212b49a88f19309b71ba19049efd86b249
Athifr <athifr56@gmail.com>
2023-08-08 15:59:52 +0700

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <video src="./video.mp4" loop autoPlay muted></video>
+      <h1>Borneo Grafisindo</h1>
+      <div className="row">
+        <button className="btn" style={{ cursor: "pointer" }}>
+          Pemesanan
+        </button>
+      </div>
+
+      <div className="headerbg"></div>
+    </header>
+  );
+}
+export default Header;
