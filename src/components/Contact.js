7abbdcbb83395ee4376c542417b041abc969c95b
HaritsUNI <100316303+HaritsUNI@users.noreply.github.com>
2023-08-18 14:54:43 +0700

    Add files via upload
    galeri

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,41 +1,146 @@
+
+
 function Contact() {
   return (
     <div className="container contact">
-      <h2 className="main-title text-center">CONTACT</h2>
-      <div className="col-md-12">
-        <div className="row">
-          <div className="col-md-4 mb-1">
-            <input name="name" placeholder="Name" className="contact-input" />
-          </div>
+      <h2 className="main-title text-center">Galeri</h2>
 
-          <div className="col-md-4 mb-1">
-            <input name="email" placeholder="Email" className="contact-input" />
+
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakA3.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 20px 10px 20px' }}>Melayani Cetak Kertas Ukuran A3+</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Kami juga melayani cetak kertas ukuran A3+ yang bisa diaplikasikan ke banyak media cetak seperti :
+              </p>
+              <p className="fst-normal" style={{ margin: '-20px 20px 10px 20px' }}>
+                Poster, Brosur,Leaflet, Flyer, Majalah, Buku, Kartu Nama, serta berbagai jenis cetakan lainnya.
+              </p>
+            </div>
           </div>
-          <div className="col-md-4 mb-1">
-            <input
-              name="subject"
-              placeholder="Subject"
-              className="contact-input"
-            />
+        </div>
+
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakKaos.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 17px 10px 20px' }}>Cetak Sablon + Kaos</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Melayani Jasa Sablon + Kaos terkait Warna Baju yang ready, Warna Hitam dan Putih, Selain Warna Hitam dan Putih
+                Harap Cek Terlebih dahulu di Agen untuk Ketersediannya.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakMiniBanner.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 20px 10px 20px' }}>Mini Banner & Standing Banner</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Melayani Cetak Mini Banner & Standing Banner. Terkait ukuran bisa disesuaikan dengan kebutuhan yang diperlukan.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <br />
-      <div className="col-md-12">
-      <textarea
-            name="message"
-            placeholder="Message"
-            className="contact-textarea"
-          />
-      </div>
 
-      <br></br>
-      <div className="row">
-        <div className="col-md-12">
-          <input className="form-btn" type="submit" value="Send Message" />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakSpanduk.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '250px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 20px 10px 20px' }}>Nota/Kwitansi & Spanduk/Banner</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Cetak Nota/Kwitansi bisa menggunakan tinta 2 warna dan 3 warna. terkait banyaknya yang ingin dicetak bisa disesuaikan dengan pesanan.
+              </p>
+              <p className="card-text" style={{ margin: '1px 25px 10px -196px' }}>
+                Cetak Spanduk/Banner bisa memilih jenis bahan dan bentuk cetak yang diinginkan bisa disesuaikan dengan pesanan yang dilakukan.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakStiker.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '20px 20px 10px 20px' }}>Stiker A3</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Menerima Cetak Stiker yang bisa disesuaikan dengan bahan dipesan baik itu : </p>
+              <p className="fw-bold" style={{ margin: '-22px 20px 10px 20px' }}>Stiker Promo, Stiker Vinyl, Stiker Crome Silver, Stiker Hologram/ Crome Pelangi, Stiker Transparant, dan Stiker Timbul.</p>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+              Untuk Lebih Lanjut bisa dilakukan pemesanan sesuai request yang diinginkan. </p>
+              
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakStand.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 20px 10px 20px' }}>Event Desk display</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Melayani desain untuk Event Desk Display. Sudah termasuk pemasangan, Desain, dan Laminasi pada Event Desk Display.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakStamp.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 20px 10px 20px' }}>Cetak Stempel Flash</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Kami juga melayani cetak Stempel + otomatis tanpa bantalan dengan berbagai jenis warna yang bisa dipilih. Serta bisa melakukan isi ulang tinta berdasarkan pesanan
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakPin.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 17px 10px 20px' }}>Gantungan Kunci, Pin, dan ID Card</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Melayani cetak Pin, Gantungan Kunci, dan ID Card. Terkait request bisa disesuaikan dengan pesanan. 
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="card" style={{ width: '30rem', display: 'flex', margin: '30px 10px' }}>
+            <img src="/img/CetakMug.jpg" className="card-img-top" alt="..." style={{ width: 'auto', height: '300px', borderRadius: '10px 20px 10px 20px', margin: '30px 20px 20px 20px' }} />
+            <div className="card-body" >
+              <h5 className="card-title" style={{ margin: '50px 20px 10px 20px' }}>Mini Banner & Standing Banner</h5>
+              <p className="card-text" style={{ margin: '10px 20px 10px 20px' }}>
+                Melayani Cetak Mini Banner & Standing Banner. Terkait ukuran bisa disesuaikan dengan kebutuhan yang diperlukan.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+
+
+
+
+
     </div>
+
+
+
+
   );
 }
 export default Contact;
